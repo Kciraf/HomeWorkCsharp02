9737f7ea93081b6d7160aeb936312c91f4f10dac
Kciraf <valsinat44a@mail.ru>
2023-02-19 01:45:31 +0600

    Added Excercise 10

--- Exercise10/Program.cs
+++ Exercise10/Program.cs
@@ -6,3 +6,13 @@
 
 System.Console.Write("Введите трехзаначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+
+if(num>99 && num < 1000)
+{
+    int result = (num / 10) % 10;
+    System.Console.WriteLine("Вторая цифра приведенного числа: " + result);
+}
+else
+{
+    System.Console.WriteLine("Введенное число не является трехзначным!");
+}
